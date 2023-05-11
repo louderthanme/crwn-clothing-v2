63165ea87ba47f8133a8ee1c5c8de1dc1dc03665
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-05-10 19:25:19 -0600

    changed folder name

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNKqXQUHGTw-ez_4cL-T6ejirrYeYEDcI",
+    authDomain: "clothing-store-572ee.firebaseapp.com",
+    projectId: "clothing-store-572ee",
+    storageBucket: "clothing-store-572ee.appspot.com",
+    messagingSenderId: "700146097333",
+    appId: "1:700146097333:web:a69084e45693e59b50f2fc"
+};
+
+const app = initializeApp(firebaseConfig);
