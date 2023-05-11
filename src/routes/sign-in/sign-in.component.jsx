e0ad570596ef5fb1d64501ea747705ebc4a91b9a
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-05-10 19:44:30 -0600

    Began google authentication through firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response)
+    }
     return (
         <div className="sign-in">
             <h1>sign in page</h1>
+            <button onClick={logGoogleUser}>sign in with google Popup</button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,10 @@
 import { initializeApp } from 'firebase/app'
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDNKqXQUHGTw-ez_4cL-T6ejirrYeYEDcI",
@@ -9,4 +15,13 @@ const firebaseConfig = {
     appId: "1:700146097333:web:a69084e45693e59b50f2fc"
 };
 
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();//needs new because it's essentially a class we get from google firebase authentication
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
