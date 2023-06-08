d58631d21bf4d3428386cb00c2997d74f9827a83
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-06-07 20:01:15 -0600

    Added aria-labels to spans for accesibility

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -19,12 +19,15 @@ const CheckoutItem = ({ cartItem }) => {
             </div>
             <span className='name'> {name}</span>
             <span className='quantity'>
-                <div className="arrow" onClick={removeItemHandler}>&#10094;</div>
+                <div className="arrow" onClick={removeItemHandler} role="button" aria-label="Decrease quantity">
+                    &#10094;
+                </div>
                 <span className='value'>
                     {quantity}
                 </span>
-                <div className="arrow" onClick={addItemHandler}> &#10095;</div>
-            </span>
+                <div className="arrow" onClick={addItemHandler} role="button" aria-label="Increase quantity">
+                    &#10095;
+                </div>            </span>
             <span className='price'> {price} </span>
             <div className='remove-button' onClick={clearItemHandler}>
                 &#10005;
