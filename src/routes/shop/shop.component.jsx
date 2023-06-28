77b1eca3f6f945b9c5a6272eecb40d4da9cb0fbe
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-06-28 13:45:16 -0600

    Before adding styled components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories.component';
+import Category from '../category/category.component';
 
 
 import './shop.styles.scss'
@@ -9,7 +10,7 @@ const Shop = () => {
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
-            <Route></Route>
+            <Route path=":category" element={<Category />} />
         </Routes>
     )
 
