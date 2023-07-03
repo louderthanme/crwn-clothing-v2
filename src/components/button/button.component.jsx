ef4e0a5110280de764e711fc072fa278bbf623e8
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-07-03 13:47:04 -0600

    + button-specific styles into styled cart-dropdown

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,4 @@
-import {BaseButton,GoogleSignInBUtton,InvertedButton} from './button.styles'
+import {BaseButton,GoogleSignInButton,InvertedButton} from './button.styles'
 
 export const BUTTON_TYPE_CLASSES = {
     base: 'base',
@@ -10,7 +10,7 @@ export const BUTTON_TYPE_CLASSES = {
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
     {
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
-        [BUTTON_TYPE_CLASSES.google]: GoogleSignInBUtton,
+        [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
     }[buttonType]
 )
--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,5 +1,12 @@
 import { styled } from "styled-components";
 
+import {
+    BaseButton,
+    GoogleSignInButton,
+    InvertedButton
+} from '../button/button.styles'
+
+
 export const CartDropdownContainer = styled.div`
     position: absolute;
     width: 240px;
@@ -12,9 +19,18 @@ export const CartDropdownContainer = styled.div`
     top: 90px;
     right: 40px;
     z-index: 5;
-    button {
+   ${BaseButton},
+   ${GoogleSignInButton},
+   ${InvertedButton} {
         margin-top: auto;
     }
+`
+
+
+
+export const EmptyMessage = styled.span`
+    font-size: 18px;
+    margin: 50px auto;
 `
 
 export const CartItems = styled.div`
